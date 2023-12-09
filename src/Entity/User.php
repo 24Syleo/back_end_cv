4394bbbe6f83d66fb303fbed8953a71876410325
Syleo24 <sylvain.beggiora@outlook.com>
2023-12-09 20:00:11 +0100

    user et migrations

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Symfony\Component\Validator\Constraints as Assert;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -22,7 +24,7 @@ class User
     #[Assert\NotBlank(message: 'Veuillez renseigner un nom.')]
     private ?string $lastname = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180, unique: true)]
     #[Assert\Email(message: 'Veuillez rentrer une adresse valide.')]
     #[Assert\NotBlank(message: 'Veuillez renseigner une adresse email.')]
     private ?string $email = null;
@@ -34,6 +36,9 @@ class User
 
     #[ORM\Column(length: 255)]
     private ?string $avatar = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
 
     public function getId(): ?int
     {
@@ -98,5 +103,39 @@ class User
         $this->avatar = $avatar;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $role[] = $this->role;
+        $role[] = 'ROLE_USER';
+
+        return array_unique($role);
+    }
+
+    public function setRoles(array $role): static
+    {
+        $this->role = $role['role'];
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
     }
 }
