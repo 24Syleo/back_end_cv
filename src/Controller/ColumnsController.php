6bd05fcaca9753f23354bcbc76a140bc39dd5f6e
24Syleo <sylvain.beggiora@outlook.com>
2023-12-11 09:24:14 +0100

    create et update column

--- src/Controller/ColumnsController.php
+++ src/Controller/ColumnsController.php
@@ -2,18 +2,55 @@
 
 namespace App\Controller;
 
+use App\Entity\Columns;
+use App\Repository\ColumnsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ColumnsController extends AbstractController
 {
-    #[Route('/api/create_columns', name: 'create_columns', methods: ['POST'])]
-    #[IsGranted('ROLE_USER')]
-    public function createColumns()
+    #[Route('/api/create_columns/{column_id}', name: 'create_columns', methods: ['POST','PUT'])]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function createColumns(?int $column_id,Request $req, ValidatorInterface $validator, EntityManagerInterface $em, ColumnsRepository $colRepo): JsonResponse
     {
-        return $this->json([
-            "columns" => "columns",
-        ]);
+        try {
+            $user = $this->getUser();
+            if ($column_id) {
+                $col = $colRepo->find($column_id);
+            } else {
+                $col  = new Columns();
+            }
+            $data = json_decode($req->getContent(), true);
+            if ($data) {
+                $col->setTitle($data['title']);
+                $col->setUser($user);
+                $col->setPosition($data['position']);
+            }
+            $errors = $validator->validate($col);
+            if (count($errors) > 0) {
+                foreach ($errors as $error) {
+                    return $this->json([
+                        "error" => $error->getMessage()
+                    ]);
+                }
+            }
+            if (!$column_id) {
+                $em->persist($col);
+            }
+            $em->flush();
+            return $this->json([
+                "column" => $col,
+            ]);
+        } catch (Exception $e) {
+            return $this->json([
+                "error" => $e,
+            ]);
+        }
     }
 }
