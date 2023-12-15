bb9cb3c5b7f6cadbd1307b748d5260a8b13323cd
24Syleo <sylvain.beggiora@outlook.com>
2023-12-15 09:55:49 +0100

    ok

--- migrations/Version20231210113519.php
+++ migrations/Version20231210113519.php
@@ -20,7 +20,7 @@ final class Version20231210113519 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE columns (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, position INT DEFAULT NULL, INDEX IDX_ACCEC0B7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE columns (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, title VARCHAR(255) NOT NULL, INDEX IDX_ACCEC0B7A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE columns ADD CONSTRAINT FK_ACCEC0B7A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
     }
 
